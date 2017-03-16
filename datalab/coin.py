f92d6d2fcff160ddd0f576c60db9f03cd090157c
RHA <laksdjf@lkasjdla.biz>
2017-03-16 15:45:17 -0500

    created database

--- datalab/coin.py
+++ datalab/coin.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__app__)
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///coins.db'
 db = SQLAlchemy(app)
 
