16ac74d029f0a422d92bff7ec77ac668e7269721
RHA <laksdjf@lkasjdla.biz>
2017-03-16 15:42:16 -0500

    made project packagey

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='datalab',
+    version='0.01.dev',
+    description='Data Lab for data',
+    author='Group 1',
+    packages=[
+        'flask',
+        'flask_sqlalchemy',
+        'requests',
+        'beautifulsoup',
+    ],
+)
